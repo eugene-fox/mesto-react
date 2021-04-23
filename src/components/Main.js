23346cb1bdb9537b91dd867ab337a6e1b1dde915
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-04-24 02:02:10 +0700

    feat/ added api func with Main.js component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,33 @@
+import React from 'react';
 import avatarPicturePath from '../images/avatar.jpg';
+import { api } from '../utils/Api';
 
 export function Main(props) {
+const [profileAvatar, setProfileAvatar] = React.useState('');
+const [profileName, setProfileName] = React.useState('');
+const [profileDescription, setProfileDescription] = React.useState('');
+
+React.useEffect(() => {
+    Promise.all([api.getUserInfo(), api.getCards()])
+    .then(([userData, cardData]) => {
+        console.log(userData);
+        setProfileAvatar(userData.avatar);
+        setProfileName(userData.name);
+        setProfileDescription(userData.about);
+    }).catch(err => console.log(err));
+}, []);
+
     return (
         <main>
             <section className="profile">
                 <div className="profile__information">
                     <div className="profile__avatar">
-                        <img className="profile__avatar-picture" src={avatarPicturePath}
+                        <img className="profile__avatar-picture" src={profileAvatar}
                             alt="Фотография Жака-Ива Кусто" />
                         <button type="button" aria-label="Изменить изображение профиля" className="profile__avatar-edit" onClick={props.onEditAvatar} />
                     </div>
-                    <h1 className="profile__name">Жак-Ив Кусто</h1>
-                    <p className="profile__description">Исследователь океана</p>
+                    <h1 className="profile__name">{profileName}</h1>
+                    <p className="profile__description">{profileDescription}</p>
                     <button type="button" aria-label="Редактировать информацию профиля" className="profile__edit-button" onClick={props.onEditProfile} />
                     <button type="button" aria-label="Добавить новую карточку" className="profile__add-button" onClick={props.onAddPlace} />
                 </div>
