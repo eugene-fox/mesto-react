0a2d771515827dd8f5fd272f96a03ae9e050894a
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-04-25 23:14:18 +0700

    fix/ edited comments in PopupWithForms.js

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-export function PopupWithForm({name, title ,buttonText, children, isOpen, onClose}) {
+export function PopupWithForm({ name, title, buttonText, children, isOpen, onClose }) {
 
-    // Доделать закрытие по Esc и оверлею
+  //TODO: Доделать закрытие по Esc и оверлею
 
-    return (
-        <section className={`popup popup_type_${name} ${isOpen && "popup_opened"}`}>
-            <form name={name} className="popup__container popup__container_profile" noValidate>
-                <h3 className="popup__title">{title}</h3>
-                {children}
-                <button type="submit" className="popup__save-button popup__save-button_profile">{buttonText}</button>
-                <button type="button" aria-label="Закрыть окно" className="popup__close-button" onClick={onClose} />
-            </form>
-        </section>
-    );
+  return (
+    <section className={`popup popup_type_${name} ${isOpen && "popup_opened"}`}>
+      <form name={name} className="popup__container popup__container_profile" noValidate>
+        <h3 className="popup__title">{title}</h3>
+        {children}
+        <button type="submit" className="popup__save-button popup__save-button_profile">{buttonText}</button>
+        <button type="button" aria-label="Закрыть окно" className="popup__close-button" onClick={onClose} />
+      </form>
+    </section>
+  );
 }
