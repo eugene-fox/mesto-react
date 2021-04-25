268d72907c236ec3a564ee354ec3656387f592a9
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-04-25 23:27:26 +0700

    fix/ close button for popup (moved out of form)

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,12 +6,14 @@ export function PopupWithForm({ name, title, buttonText, children, isOpen, onClo
 
   return (
     <section className={`popup popup_type_${name} ${isOpen && "popup_opened"}`}>
-      <form name={name} className="popup__container popup__container_profile" noValidate>
-        <h3 className="popup__title">{title}</h3>
-        {children}
-        <button type="submit" className="popup__save-button popup__save-button_profile">{buttonText}</button>
+      <div className="popup__container popup__container_profile">
+        <form name={name} noValidate>
+          <h3 className="popup__title">{title}</h3>
+          {children}
+          <button type="submit" className="popup__save-button popup__save-button_profile">{buttonText}</button>
+        </form>
         <button type="button" aria-label="Закрыть окно" className="popup__close-button" onClick={onClose} />
-      </form>
+      </div>
     </section>
   );
 }
